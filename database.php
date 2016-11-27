a44276e674dfdb9b4ee3ebf3ab97b8c6bd317bec
Ethan <egccohn@gmail.com>
2016-11-27 18:28:17 -0500

    aaaahhhhh

--- database.php
+++ database.php
@@ -1,14 +1,11 @@
 <?php
-
 	$server = 'localhost';
 	$username = 'root';
 	$password = 'root';
 	$database = 'users_database';
-
 	try {
 		$conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
 	} 
 	catch (PDOException $e) {
 		die("Connection to database failed." . $e->getMessage());
 	}
-
--- index.php
+++ index.php
@@ -1,39 +1,84 @@
 <?php
 
- session_start();
+    require 'database.php';
+    session_start();
+    if( isset($_SESSION['user_id']) ){
+        header("Location: home.html");
+            exit;
+    }
 
-require 'database.php';
+    $error = false;
+    $message = '';
 
-if( isset($_SESSION['user_id']) ){
+    if (isset($_POST['submit'])) {
+        if (empty($_POST['username']) || empty($_POST['password'])) {
+            $error = true;
+            $message = "Please fill out all fields.";
+        } 
 
-	$records = $conn->prepare('SELECT id,email,password FROM users WHERE id = :id');
-	$records->bindParam(':id', $_SESSION['user_id']);
-	$records->execute();
-	$results = $records->fetch(PDO::FETCH_ASSOC);
+        if (!$error) {
 
-	$user = NULL;
+            $query = 'SELECT user_id,username,password FROM users WHERE username = :username';
+            $records = $conn->prepare($query);
+            $records->bindParam(':username', $_POST['username']);
+            $records->execute();
+            $results = $records->fetch(PDO::FETCH_ASSOC);
 
-	if( count($results) > 0){
-		$user = $results;
-	}
+            if(count($results) > 0 && $_POST['password']==$results['password']  ){
 
-}
+                $_SESSION['user_id'] = $results['user_id'];
+                header("Location: home.html");
 
+                    exit;
+            } else {
+                    
+                    $error = true;
+                    $message = "Sorry, those credentials do not match.";
+                }
+            
+        } 
+    }     
+
+    
+        
 ?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Opinionator</title>
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="bootstrap-4.0.0/css/bootstrap.css">
 </head>
 <body>
 
+	<div class="content">
 
-	<h1>Welcome to Opinionator! </h1><br>
-	<a href="login.php">Login </a> or 
-	<a href="register.php">Register </a><br>
-
+        <a class="link link--nukun" href="#">Op<span>ini</span>on</a>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 ">
+                    <div class="jumbotron" id="msg">
+                        <h1>Login</h1>
+                        <form action="index.php" method="POST">
+							<input type="text" placeholder="username" name="username"> <br>
+							<input type="password" placeholder="password" name="password"> <br><br>
+							<input type="submit" name="submit"> <br><br>
+						</form>
+                        <?php
+                       if ( isset($message) ) { ?>
+                            <p><?= $message ?></p>
+                        <?php } ?>
+						or <a href="register.php">Register</a>
+                    </div>
+                     
+                </div>
+                
+            </div>       
+        </div>
+    </div>
 
 </body>
 </html>
