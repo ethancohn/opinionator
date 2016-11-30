981120cb52ddfaed2a860edf9df2e14d9296a48e
Tony Chen <t.z.chen96@gmail.com>
2016-11-30 18:34:24 -0500

    changed user_id to username for fetching

--- convos_following.sql
+++ convos_following.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
 --
--- Host: localhost:8889
--- Generation Time: Nov 30, 2016 at 04:59 AM
--- Server version: 5.6.33
--- PHP Version: 7.0.12
+-- Host: localhost
+-- Generation Time: Nov 30, 2016 at 01:27 AM
+-- Server version: 5.7.13-0ubuntu0.16.04.2
+-- PHP Version: 7.0.8-0ubuntu0.16.04.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `opinionator_database`
@@ -21,6 +27,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `convos_following` (
-  `user_id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
   `convo_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `convos_following`
+--
+
+INSERT INTO `convos_following` (`username`, `convo_id`) VALUES
+('tony', 20),
+('tony', 21),
+('tony', 2);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
